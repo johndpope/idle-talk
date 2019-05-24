14dff4b35da6878a0281d13d4bd8a088fdb74123
SungmanYou <sungman.you@gmail.com>
2019-05-24 17:52:45 +0900

    Prerequisites for Google Sign In

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,28 @@
 
 <script>
 export default {
-  name: "App"
+  name: 'App',
+  meta: {
+    meta: {
+      googleSignInClientId: {
+        name: 'google-signin-client_id',
+        content: '273389259899-cjflhfg1s347huh7ulv3oug8b9bc0nuk.apps.googleusercontent.com' // prettier-ignore
+      }
+    },
+    link: {},
+    script: {
+      googlePlatformLibrary: {
+        src: 'https://apis.google.com/js/platform.js',
+        async: true,
+        defer: true
+      }
+    },
+    htmlAttr: {},
+    bodyAttr: {},
+    noscript: {
+      default: 'This is content for browsers with no JS (or disabled JS)'
+    }
+  }
 };
 </script>
 
